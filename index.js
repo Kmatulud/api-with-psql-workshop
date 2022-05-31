3401bab6cd6a9aab8b7415ccd4087cadad760998
kmatulud <princessmakgotso@gmail.com>
2022-05-31 22:31:48 +0200

    deploy

--- index.js
+++ index.js
@@ -20,22 +20,13 @@ app.use(cors());
 const DATABASE_URL = process.env.DATABASE_URL
 const pgp = PgPromise({});
 
-
-// const config = {
-// 	connectionString: DATABASE_URL,
-// };
-
-// if (process.env.NODE_ENV !== 'development') {
-// 	config.ssl = { rejectUnauthorized: false };
-// }
-
 const config = {
-	connectionString:
-		process.env.DATABASE_URL ||
-		"postgres://gary:gar123@localhost:5432/garment_app",
-	max: 30,
-	ssl: { rejectUnauthorized: false },
+	connectionString: DATABASE_URL,
 };
+
+if (process.env.NODE_ENV !== 'development') {
+	config.ssl = { rejectUnauthorized: false };
+}
 
 const db = pgp(config);
 
